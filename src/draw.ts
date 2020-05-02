5f1cef3d83b0b63c0f776659034fcbe60fe41862
Theo Bonnard <theo.bonnard@supelec.fr>
2020-05-02 18:26:05 +0200

    Wavy oscillating or running circles and debug circle

--- src/draw.ts
+++ src/draw.ts
@@ -24,20 +24,43 @@ export function plotSine(ctx: CanvasRenderingContext2D, xOffset: number, yOffset
 }
 
 export function plotCircle(ctx: CanvasRenderingContext2D, radius: number, step: number) {
+    const frequency = 12;
+    const phase = degreesToRadians(step);
+    const shapeFactor = 5;
+
+    // Running wavy circle
     ctx.beginPath()
     ctx.lineWidth = 4;
     ctx.strokeStyle = "rgb(66,44,255)";
-
-    const frequency = 3;
-    const phase = degreesToRadians(step);
-    const amplitude = angle => (radius / 5) * Math.cos(angle * frequency)
+    const amplitude = angle => Math.cos(angle * frequency) / shapeFactor
     Array.from(Array(360).keys())   // every degree
         .map(degree => degreesToRadians(degree))    // rad
         .map(rad => ({
-            x: amplitude(rad + phase) + radius * Math.cos(rad),
-            y: amplitude(rad + phase) + radius * Math.sin(rad)
+            x: radius * (amplitude(rad + phase) + 1) * Math.cos(rad),     // radius * (s(t,theta) + const) * polar2cart
+            y: radius * (amplitude(rad + phase) + 1) * Math.sin(rad)
         }))
         .forEach(point => ctx.lineTo(point.x, point.y))
+    ctx.stroke()
 
+    // Oscillating wavy circle
+    ctx.beginPath()
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "rgb(66,44,255)";
+    Array.from(Array(360).keys())   // every degree
+        .map(degree => degreesToRadians(degree))    // rad
+        .map(rad => ({ theta: rad, fixedShape: Math.cos(4 * rad) }))
+        .forEach(coord => {
+            ctx.lineTo(
+                radius * (Math.cos(phase) * coord.fixedShape / shapeFactor + 1) * Math.cos(coord.theta),     // radius * (f(t) * g(theta) + const) * polar2cart
+                radius * (Math.cos(phase) * coord.fixedShape / shapeFactor + 1) * Math.sin(coord.theta)
+            )
+        })
+    ctx.stroke()
+
+    // DEBUG CIRCLE
+    ctx.beginPath()
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "blue";
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI)
     ctx.stroke()
 }
