3976eb881cd000ed9a5c3b16fb1fc15476e9a421
Theo Bonnard <theo.bonnard@supelec.fr>
2020-05-03 18:35:12 +0200

    Extract boilerplate from draw function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { plotCircle, oscillatingCircle, runningCircle } from "./draw";
 
+const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const potar1 = <HTMLInputElement>document.getElementById('potar1')
+
+canvas.style.border = '1px solid #63eccd';
+canvas.width = window.innerWidth * 2 / 3;
+canvas.height = window.innerHeight * 4 / 5;
+
+ctx.translate(canvas.width / 2, canvas.height / 2)
+
 var frame = 0;  // Animation frame
 init();
 
@@ -8,24 +18,16 @@ function init() {
 }
 
 function draw() {
-    const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const potar1 = (<HTMLInputElement>document.getElementById('potar1')).valueAsNumber
-
-    canvas.style.border = '1px solid #63eccd';
-    canvas.width = window.innerWidth * 2 / 3;
-    canvas.height = window.innerHeight * 4 / 5;
-
-    ctx.translate(canvas.width / 2, canvas.height / 2)
     ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
     // context.save();
 
+    // Debug circle
     plotCircle(ctx, 200, frame);
 
     // Running wavy circle
     runningCircle(ctx, frame)
     // Oscillating wavy circle
-    oscillatingCircle(ctx, frame, 200, potar1)
+    oscillatingCircle(ctx, frame, 200, potar1.valueAsNumber)
 
     // context.restore();
     frame++;
