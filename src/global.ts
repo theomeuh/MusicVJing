86bc3fd187faef47475ae1cb6362bd09c00f2ece
Theo Bonnard <theo.bonnard@supelec.fr>
2020-05-13 16:54:52 +0200

    Add a new canvas for "debug"
    In fact it will serve as a second screen to manage the viz

--- src/global.ts
+++ src/global.ts
@@ -4,6 +4,9 @@
 export const canvas = <HTMLCanvasElement>document.getElementById('canvas');
 export const canvasCtx = canvas.getContext('2d');
 
+export const debugCanvas = <HTMLCanvasElement>document.getElementById('debug-canvas');
+export const debugCanvasCtx = debugCanvas.getContext('2d');
+
 // control
 // range is [0..100]. Their values should be wrapped 
 export const potar1 = <HTMLInputElement>document.getElementById('potar1');
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 // entrypoint
 
-import { canvas, canvasCtx, potar1 } from "./global";
+import { canvas, canvasCtx, debugCanvas, potar1, potar2 } from "./global";
 import { runningCircleFrequencySweepViz, runningCircleMicViz } from "./preset";
 
 canvas.style.border = '1px solid #63eccd';
 canvas.width = window.innerWidth * 9 / 10;
-canvas.height = window.innerHeight * 4 / 5;
-
+canvas.height = window.innerHeight * 16 / 20;
 canvasCtx.translate(canvas.width / 2, canvas.height / 2)
+
+debugCanvas.style.border = '1px solid red';
+debugCanvas.width = window.innerWidth * 9 / 10;
+debugCanvas.height = window.innerHeight * 3 / 20;
 
 var frame = 0;  // Animation frame
 
 runningCircleMicViz(frame, potar1);
-// runningCircleFrequencySweepViz(frame, potar1);
+// runningCircleFrequencySweepViz(frame, potar2);
